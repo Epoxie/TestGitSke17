58c5a0b8fe8daa2b17ddceecd9cef5bcb314ca6c
Magnus <Olchardwell@hotmail.com>
2017-10-09 10:13:08 +0200

    added readkey

--- TestGit/TestGit/Program.cs
+++ TestGit/TestGit/Program.cs
@@ -11,6 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("wow hello Github or something.");
+            Console.ReadKey();
         }
     }
 }
